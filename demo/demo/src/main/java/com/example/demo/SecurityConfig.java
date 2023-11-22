e7b668c5b829b6c5d67e86f74ea98c46b6be5e5f
ManiChandana95 <55896122+ManiChandana95@users.noreply.github.com>
2023-11-22 19:47:05 +0530

    Update and rename DemoApplication.java to SecurityConfig.java

---
+++ demo/demo/src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,23 @@
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    // Other necessary autowired components or services
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/public").permitAll()
+            .antMatchers("/api/private").hasRole("USER")
+            .antMatchers("/api/admin").hasRole("ADMIN")
+            .anyRequest().authenticated()
+            .and()
+            .apply(new JwtConfigurer(jwtTokenProvider));
+    }
+    
+    // Other configurations and beans
+}
