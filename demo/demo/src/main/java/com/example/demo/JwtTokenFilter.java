3589783acbc9fe720ed6778857c6e03a2fef68d0
ManiChandana95 <55896122+ManiChandana95@users.noreply.github.com>
2023-11-22 19:49:18 +0530

    Create JwtTokenFilter.java

---
+++ demo/demo/src/main/java/com/example/demo/JwtTokenFilter.java
@@ -0,0 +1,10 @@
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Implement token processing logic
+    }
+}
