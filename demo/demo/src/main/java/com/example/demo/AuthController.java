f26b2064db443c52866d2a0a7bb93aabeab70dc8
ManiChandana95 <55896122+ManiChandana95@users.noreply.github.com>
2023-11-22 20:46:40 +0530

    Update AuthController.java

--- demo/demo/src/main/java/com/example/demo/AuthController.java
+++ demo/demo/src/main/java/com/example/demo/AuthController.java
@@ -14,4 +14,22 @@ public class AuthController {
         // Generate JWT token using jwtTokenProvider.generateToken(userDetails)
         // Return the generated token in the response
     }
+    @PostMapping("/refresh")
+public ResponseEntity<?> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+    // Validate the refresh token
+    // Generate a new JWT token (if refresh token is valid) using jwtTokenProvider.generateToken(userDetails)
+    // Return the new token in the response
+}
+@PostMapping("/register")
+public ResponseEntity<?> registerUser(@RequestBody RegistrationRequest registrationRequest) {
+    // Validate and process registration request (create new user, store in database, etc.)
+    // Return success response or appropriate message
+}
+@PostMapping("/logout")
+public ResponseEntity<?> logout(HttpServletRequest request) {
+    // Invalidate the JWT token (if needed)
+    // Perform logout actions if necessary
+    // Return success response or appropriate message
+}
+
 }
