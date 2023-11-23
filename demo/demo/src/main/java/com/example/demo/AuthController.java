c44c3a945162a5b4b2677c51fffa4d3a25b47053
ManiChandana95 <55896122+ManiChandana95@users.noreply.github.com>
2023-11-23 19:23:28 +0530

    Update AuthController.java

--- demo/demo/src/main/java/com/example/demo/AuthController.java
+++ demo/demo/src/main/java/com/example/demo/AuthController.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -20,6 +22,12 @@ public class AuthController {
     
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private UserRepository userRepository; // Assuming UserRepository handles database operations for users
+
+    @Autowired
+    private PasswordEncoder passwordEncoder; // Assuming PasswordEncoder bean is configured
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
@@ -60,6 +68,21 @@ public ResponseEntity<?> refreshToken(@RequestBody RefreshTokenRequest refreshTo
 public ResponseEntity<?> registerUser(@RequestBody RegistrationRequest registrationRequest) {
     // Validate and process registration request (create new user, store in database, etc.)
     // Return success response or appropriate message
+    // Check if the username is already taken
+        if (userRepository.existsByUsername(registrationRequest.getUsername())) {
+            return ResponseEntity.badRequest().body("Username is already taken!");
+        }
+
+        // Create a new user entity
+        User user = new User();
+        user.setUsername(registrationRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registrationRequest.getPassword()));
+        // Set other user details as needed
+
+        // Save the new user to the database
+        userRepository.save(user);
+
+        return ResponseEntity.ok("User registered successfully!");
 }
 @PostMapping("/logout")
 public ResponseEntity<?> logout(HttpServletRequest request) {
@@ -85,4 +108,11 @@ public class JwtAuthenticationResponse {
     }
 
     // getter and setter
+}
+public class RegistrationRequest {
+
+    private String username;
+    private String password;
+
+    // getters and setters
 }
