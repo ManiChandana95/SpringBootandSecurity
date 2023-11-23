eb79ebc5f38f61e83114cde2830b9d99e21d937f
ManiChandana95 <55896122+ManiChandana95@users.noreply.github.com>
2023-11-23 17:32:16 +0530

    Update JwtTokenProvider.java

--- demo/demo/src/main/java/com/example/demo/JwtTokenProvider.java
+++ demo/demo/src/main/java/com/example/demo/JwtTokenProvider.java
@@ -2,9 +2,26 @@
 public class JwtTokenProvider {
 
     // Autowire necessary components or services
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
     
     public String generateToken(UserDetails userDetails) {
         // Implement token generation logic
+        Map<String, Object> claims = new HashMap<>();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
     }
 
     public boolean validateToken(String token) {
