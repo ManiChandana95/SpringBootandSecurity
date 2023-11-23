c66ae4e3bfd41a8198e69d6697b28070597f19b1
ManiChandana95 <55896122+ManiChandana95@users.noreply.github.com>
2023-11-23 17:14:46 +0530

    Update CustomUserDetailsService.java

--- demo/demo/src/main/java/com/example/demo/CustomUserDetailsService.java
+++ demo/demo/src/main/java/com/example/demo/CustomUserDetailsService.java
@@ -1,5 +1,22 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    
+    @Autowired
+    private UserRepository userRepository; // Assuming UserRepository handles database operations for users
+
     // Implement loadUserByUsername method to fetch user details
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return CustomUserDetails.build(user);
+    }
 }
