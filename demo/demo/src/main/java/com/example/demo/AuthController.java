a46ea6c379e606d01f3972cbc8ae87ea7939c1aa
ManiChandana95 <55896122+ManiChandana95@users.noreply.github.com>
2023-11-23 19:09:06 +0530

    Update AuthController.java

--- demo/demo/src/main/java/com/example/demo/AuthController.java
+++ demo/demo/src/main/java/com/example/demo/AuthController.java
@@ -1,3 +1,13 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
@@ -13,6 +23,13 @@ public class AuthController {
         // Authenticate user (use authenticationManager.authenticate())
         // Generate JWT token using jwtTokenProvider.generateToken(userDetails)
         // Return the generated token in the response
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtTokenProvider.generateToken((CustomUserDetails) authentication.getPrincipal());
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
     }
     @PostMapping("/refresh")
 public ResponseEntity<?> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
