2a6cc256272f756524123f3e9cd6c710a80cb127
ManiChandana95 <55896122+ManiChandana95@users.noreply.github.com>
2023-11-23 19:55:27 +0530

    Create MainClass.java

---
+++ demo/demo/src/main/java/com/example/demo/MainClass.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass { // Replace with your preferred name
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainClass.class, args);
+    }
+}
